721b7b475c9ec7215e81b9f85379c978d10ce15a
Will Mallett <mallett002@gmail.com>
2021-05-04 06:51:53 -0500

    adding hapi

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// playing with this library for learning
-const { Chess } = require('chess.js');
+import init from './server';
 
-const chess = new Chess();
-// const board = chess.board();
-// console.log(board);
-
-chess.move('a4');
-chess.move('Nc6'); // if special piece, need to prepend its letter
-chess.move('d4');
-
-console.log('before move: ', chess.ascii());
-const moves = chess.moves({square: 'c6', verbose: true}); // can use it's "san" key for standard algebraic notation
-// console.log({moves});
-
-const moveToCell = moves.find((move) => move.to === 'b8');
-console.log('moving to: ', moveToCell);
-chess.move(moveToCell.san);
-
-console.log(chess.ascii());
-// console.log('turn: ', chess.turn());
+init().then((console.log('running!!')));
---
+++ server.js
@@ -0,0 +1,19 @@
+import Hapi from '@hapi/hapi';
+
+const init = async () => {
+  const server = Hapi.server({
+      port: 3000,
+      host: 'localhost'
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+export default init;
