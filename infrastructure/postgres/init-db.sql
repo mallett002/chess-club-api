adc056aa7cbb868f1158400174f0dbdeaaba7f70
Will Mallett <mallett002@gmail.com>
2021-08-28 07:57:33 -0500

    getting migrations to work

--- infrastructure/postgres/init-db.sql
+++ infrastructure/postgres/init-db.sql
@@ -1,8 +1,6 @@
--- Todo: create roles and chess_club_api database
--- flyway and chess_club_
-CREATE ROLE flyway LOGIN PASSWORD 'flyway_ps' CREATEDB CREATEROLE
-CREATE ROLE chess_club_api LOGIN PASSWORD 'membership_api_ps'
+CREATE ROLE flyway LOGIN PASSWORD 'flyway_ps' CREATEDB CREATEROLE REPLICATION;
+CREATE ROLE chess_club_api LOGIN PASSWORD 'chess_club_api_ps' CREATEDB CREATEROLE INHERIT;
 
-CREATE DATABASE "chess-club-api"
+CREATE DATABASE "chess-club-api";
 
-GRANT ALL ON DATABASE chess-club-api TO flyway
+GRANT ALL ON DATABASE "chess-club-api" TO flyway;
--- scripts/flyway-migrate.sh
+++ scripts/flyway-migrate.sh
@@ -2,8 +2,7 @@
 
 flyway_script_path=$(cd "$(dirname "${0}")" ; cd ../infrastructure/postgres ; pwd -P)
 
-echo "migrating database"
 
-flyway migrate -user=flyway -password=flyway_ps -connectRetries=10 -url='jdbc:postgresql://127.0.0.1:5432/chess-club-api' -locations=filesystem:${flyway_script_path}/migrations
+# flyway migrate -user=flyway -password=flyway_ps -connectRetries=10 -url='jdbc:postgresql://127.0.0.1:5432/chess-club-api' -locations=filesystem:${flyway_script_path}/migrations
 
-echo "finished migration."
+flyway -user=flyway -password=flyway_ps -url=jdbc:postgresql://127.0.0.1:5432/chess-club-api -locations=filesystem:${flyway_script_path}/migrations migrate
