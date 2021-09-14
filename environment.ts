2e7a436b1f28a42d1c936a08d83515ded3659d45
Will Mallett <mallett002@gmail.com>
2021-09-14 06:04:27 -0500

    allow db to connect in docker

--- environment.ts
+++ environment.ts
@@ -5,7 +5,8 @@ interface Environment {
     introspection: boolean,
     playground: boolean
   },
-  port: number|string;
+  port: number|string,
+  postgresHost: string
 }
 
 export const env: Environment = {
@@ -13,5 +14,6 @@ export const env: Environment = {
     introspection: process.env.NODE_ENV !== 'production',
     playground: process.env.NODE_ENV !== 'production'
   },
+  postgresHost: process.env.POSTGRES_HOST || 'localhost',
   port: process.env.PORT || defaultPort
 };
