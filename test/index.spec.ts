d576f6ac3fbebbd4bc29f7f21f856333349bb51d
williammallett <37602780+mallett002@users.noreply.github.com>
2021-10-02 07:52:04 -0400

    Config (#16)
    * WIP: Working on adding in config
    
    * Get the app working with config
    
    * remove unused env vars
    
    * update index.ts test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,22 +1,27 @@
 import Chance from 'chance';
+import config from 'config';
 import { ApolloServer, gql } from 'apollo-server';
 
-import { env } from '../environment';
 import ChessClubDatabase from '../src/repository/chess-club-database';
 import { resolvers } from '../src/resolvers/resolver-map';
 
 jest.mock('../src/repository/chess-club-database');
 jest.mock('apollo-server');
+jest.mock('config');
 
 const chance = new Chance();
 describe('index', () => {
   const DatabaseMock = ChessClubDatabase as jest.MockedClass<typeof ChessClubDatabase>;
   const ApolloMock = ApolloServer as jest.MockedClass<typeof ApolloServer>;
   const gqlMock = gql as jest.MockedFunction<typeof gql>;
+  const configGetMock = config.get as jest.MockedFunction<typeof config.get>;
 
   let dbInstance,
     server,
     serverUrl,
+    chessClubDBConnection,
+    apolloConfig,
+    port,
     typeDefs;
 
   beforeAll(() => {
@@ -26,6 +31,18 @@ describe('index', () => {
       listen: jest.fn()
     };
     serverUrl = chance.url();
+    chessClubDBConnection = {
+      database: 'chess-club-api',
+      host: 'localhost',
+      password: 'chess_club_api_ps',
+      port: 5432,
+      user: 'chess_club_api'
+    };
+    apolloConfig = {
+      introspection: chance.bool(),
+      playground: chance.bool()
+    };
+    port = chance.natural();
 
     DatabaseMock.mockReturnValue(dbInstance);
     gqlMock.mockReturnValue(typeDefs);
@@ -33,37 +50,39 @@ describe('index', () => {
     server.listen.mockResolvedValue({
       url: serverUrl
     });
+    configGetMock.mockReturnValueOnce(chessClubDBConnection);
+    configGetMock.mockReturnValueOnce(apolloConfig);
+    configGetMock.mockReturnValue(port);
 
     require('../index');
   });
 
   it('should start the server', () => {
+    expect(config.get).toHaveBeenCalledTimes(3);
+    expect(config.get).toHaveBeenNthCalledWith(1, 'chess_club_db');
+    expect(config.get).toHaveBeenNthCalledWith(2, 'apollo');
+    expect(config.get).toHaveBeenNthCalledWith(3, 'port');
+
     expect(DatabaseMock).toHaveBeenCalledTimes(1);
     expect(DatabaseMock).toHaveBeenCalledWith({
       client: 'pg',
-      connection: {
-        database: 'chess-club-api',
-        host: 'localhost',
-        password: 'chess_club_api_ps',
-        port: 5432,
-        user: 'chess_club_api'
-      }
+      connection: chessClubDBConnection
     });
     expect(ApolloMock).toHaveBeenCalledTimes(1);
     expect(ApolloMock).toHaveBeenCalledWith({
       resolvers,
       typeDefs,
       dataSources: expect.any(Function),
-      introspection: true,
-      playground: true,
+      introspection: apolloConfig.introspection,
+      playground: apolloConfig.playground
     });
 
     const dataSources = ApolloMock.mock.calls[0][0].dataSources;
     const result = dataSources();
 
-    expect(result).toStrictEqual({chessClubDatabase: dbInstance});
+    expect(result).toStrictEqual({ chessClubDatabase: dbInstance });
 
     expect(server.listen).toHaveBeenCalledTimes(1);
-    expect(server.listen).toHaveBeenCalledWith({port: env.port});
+    expect(server.listen).toHaveBeenCalledWith({ port });
   });
 });
