6b25b7e002a7f1d2cf61e080006b72689969a5fe
Will Mallett <mallett002@gmail.com>
2021-10-06 06:15:34 -0500

    got the server working with subscriptions/express

--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import config from 'config';
-// import bodyParser from 'body-parser';
 import http from 'http';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-// import { execute, subscribe } from 'graphql';
-// import { SubscriptionServer } from 'subscriptions-transport-ws';
-// import ws from 'ws';
-// import { useServer } from 'graphql-ws/lib/use/ws';
+import ws from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 
 import ChessClubDatabase from './src/repository/chess-club-database';
 import { resolvers } from './src/resolvers/resolver-map';
@@ -26,7 +23,7 @@ async function startApolloServer(typeDefs, resolvers) {
   const app = express();
   const httpServer = http.createServer(app);
 
-  const server = new ApolloServer({
+  const apolloServer = new ApolloServer({
     dataSources: () => ({ chessClubDatabase }),
     typeDefs,
     resolvers,
@@ -34,60 +31,23 @@ async function startApolloServer(typeDefs, resolvers) {
     introspection: apolloConfig.introspection
   });
 
-  await server.start();
+  await apolloServer.start();
 
-  server.applyMiddleware({
+  apolloServer.applyMiddleware({
      app,
      path: '/graphql'
   });
 
-  await Promise.resolve(httpServer.listen({ port }));
+  const server = httpServer.listen(port, () => {
+    const wsServer = new ws.Server({
+      server,
+      path: '/graphql',
+    });
+  
+    useServer({ schema: typeDefs }, wsServer);
+  });
 
-  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
+  console.log(`🚀 Server ready at http://localhost:${port}${apolloServer.graphqlPath}`);
 }
 
 startApolloServer(typeDefs, resolvers);
-
-// old stuff:
-// const app = express();
-
-// app.use('/graphql', bodyParser.json());
-
-// const knexConfig = {
-//   client: 'pg',
-//   connection: config.get('chess_club_db')
-// };
-// const apolloConfig = config.get('apollo');
-// const port = config.get('port');
-
-// const chessClubDatabase = new ChessClubDatabase(knexConfig);
-
-// let apolloServer = null;
-
-// async function startApolloServer() {
-//   apolloServer = new ApolloServer({
-//     resolvers,
-//     schema: typeDefs,
-//     dataSources: () => ({ chessClubDatabase }),
-//     introspection: apolloConfig.introspection
-//   });
-
-//   await apolloServer.start();
-
-//   apolloServer.applyMiddleware({ app });
-// }
-
-// startApolloServer();
-
-// // const server = createServer(app);
-
-// const httpServer = app.listen(port, () => {
-//   const wsServer = new ws.Server({
-//     server: app,
-//     path: '/graphql',
-//   });
-
-//   useServer({ schema: typeDefs }, wsServer);
-
-//   console.log("GraphQL server listening on port %s", port);
-// });
