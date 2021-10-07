e0ad6de78831782373482c76f648d7017e45d4a2
Will Mallett <mallett002@gmail.com>
2021-10-07 06:52:30 -0500

    notes for signup flow

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,4 +2,13 @@ export const applyServerRoutes = (app) => {
   app.get('/health', (req, res) => {
     res.send('healthy');
   });
+
+  app.post('create-account', (req, res) => {
+    /*
+      - Take in a username, password
+      - encrypt the password
+      - store the username and password in the DB.
+      - authenticate the user (give them a JWT)
+    */
+  });
 }
