e2d36e125fa1737fe4fca841ab9801669a3cd233
Will Mallett <mallett002@gmail.com>
2021-10-30 09:16:10 -0500

    get verify passing with health check

--- scripts/health.mjs
+++ scripts/health.mjs
@@ -1,28 +1,46 @@
 import fetch from 'node-fetch';
 
-const maxRetries = 10;
+const maxRetries = 20;
 const backOffInMilis = 500;
 
 let retryCount = 0;
 
+async function retry() {
+  retryCount++;
+
+  if (retryCount > maxRetries) {
+    console.log('chess-club-api did not come up....');
+    process.exit(1);
+  }
+  
+  console.log(`Retrying chess-club-api /health ${retryCount}/${maxRetries}`);
+
+  const exponentialBackOff = backOffInMilis * retryCount;
+
+  setTimeout(async () => await isAppUpAndReady(), exponentialBackOff);
+  
+}
+
 async function isAppUpAndReady() {
-  const response = await fetch('http://localhost:4000/health');
+  let response;
+  
+  try {
+    response = await fetch('http://localhost:4000/health');
 
-  if (!response.ok) {
-    retryCount++;
-
-    if (retryCount > maxRetries) {
-      throw new Error('chess-club-api did not come up.');
+    if (!response.ok) {
+      await retry();
     }
-    
-    console.log(`Retrying chess-club-api /health ${retryCount}/${maxRetries}`);
 
-    const exponentialBackOff = backOffInMilis * retryCount;
+    const json = await response.json();
+  
+    if (!json.postgresHealthy) {
+      await retry();
+    }
 
-    setTimeout(async () => await isAppUpAndReady(), exponentialBackOff);
+    return;
+  } catch (error) {
+    await retry();
   }
 }
 
-isAppUpAndReady().then(() => {
-  console.log('Up and ready!');
-});
+isAppUpAndReady();
