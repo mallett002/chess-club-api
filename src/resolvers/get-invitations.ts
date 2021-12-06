c6bb31287474d99f81e75774b26c8d7f2b031cd7
Will Mallett <mallett002@gmail.com>
2021-12-06 06:56:11 -0600

    Notes

--- src/resolvers/get-invitations.ts
+++ src/resolvers/get-invitations.ts
@@ -3,10 +3,13 @@ import { PlayerJwtPayload } from 'jsonwebtoken';
 
 import { IToken } from '../interfaces/account';
 import { IBoard } from '../interfaces/board';
+import { IGameInvites } from '../interfaces/invitation';
 import { verifyToken } from '../services/accounts/token-service';
 import { getInvitationsAndInboundRequests } from '../services/invitation';
 
-export default async (parent, args, context: IToken) => {
+
+// Todo: change GameRequests nomenclature to Invitation
+export default async (parent, args, context: IToken): Promise<IGameInvites> => {
   const claims: PlayerJwtPayload = verifyToken(context);
 
   if (!claims) {
