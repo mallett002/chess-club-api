3199daac7e825133c2bae856ee978eff1ad7df72
Will Mallett <mallett002@gmail.com>
2022-02-19 09:09:43 -0600

    passing create-game spec

--- acceptance/tests/create-game.spec.ts
+++ acceptance/tests/create-game.spec.ts
@@ -54,8 +54,6 @@ describe('create game', () => {
       invitationId: invitation.invitationId
     });
 
-    console.log({invitation});
-    
     const expectedPlayerOne = inviteeColor === 'w' ? secondPlayer.player_id : firstPlayer.player_id;
 
     expect(response.createGame.gameId).toBeDefined();
@@ -63,54 +61,44 @@ describe('create game', () => {
     expect(response.errors).toBeUndefined();
   });
 
-  // it('should delete the invitation after the game is created', async () => {
-  //   let invitations = await selectInvitations();
-  //   let foundInvitation = invitations.find((it) => it.invitation_id === invitation.invitationId);
+  it('should delete the invitation after the game is created', async () => {
+    let invitations = await selectInvitations();
+    let foundInvitation = invitations.find((it) => it.invitation_id === invitation.invitationId);
 
-  //   expect(foundInvitation).toBeDefined();
+    expect(foundInvitation).toBeDefined();
 
-  //   await gqlClient.request(createGameMutation, {
-  //     invitationId: invitation.invitationId,
-  //     inviteeColor: chance.pickone(['w', 'b'])
-  //   });
+    await gqlClient.request(createGameMutation, {
+      invitationId: invitation.invitationId,
+      inviteeColor: chance.pickone(['w', 'b'])
+    });
 
-  //   invitations = await selectInvitations();
-  //   foundInvitation = invitations.find((it) => it.invitation_id === invitation.invitationId);
+    invitations = await selectInvitations();
+    foundInvitation = invitations.find((it) => it.invitation_id === invitation.invitationId);
 
-  //   expect(foundInvitation).toBeUndefined();
-  // });
+    expect(foundInvitation).toBeUndefined();
+  });
 
-  // it('should throw a validation error if an arg is missing', async () => {
-  //   const argToDelete = chance.pickone(['invitationId', 'inviteeColor']);
-  //   const args = {
-  //     invitationId: invitation.invitationId,
-  //     inviteeColor: chance.pickone(['w', 'b'])
-  //   };
-  //   const argTypes = {invitationId: 'ID!', inviteeColor: 'String!'};
+  it('should throw a validation error if an arg is missing', async () => {
+    try {
+      await gqlClient.request(createGameMutation);
+      throw new Error('Should have failed.');
+    } catch (error) {
+      expect(error.response.errors[0].extensions.code).toStrictEqual('BAD_USER_INPUT');
+      expect(error.message).toContain(`Variable \"$invitationId\" of required type \"ID!\" was not provided.`);
+    }
+  });
 
-  //   delete args[argToDelete];
+  it('should throw an auth error if not authenticated', async () => {
+    gqlClient = new GraphQLClient(graphqlUrl);
 
-  //   try {
-  //     await gqlClient.request(createGameMutation, args);
-  //     throw new Error('Should have failed.');
-  //   } catch (error) {
-  //     expect(error.response.errors[0].extensions.code).toStrictEqual('BAD_USER_INPUT');
-  //     expect(error.message).toContain(`Variable \"$${argToDelete}\" of required type \"${argTypes[argToDelete]}\" was not provided.`);
-  //   }
-  // });
-
-  // it('should throw an auth error if not authenticated', async () => {
-  //   gqlClient = new GraphQLClient(graphqlUrl);
-
-  //   try {
-  //     await gqlClient.request(createGameMutation, {
-  //       invitationId: invitation.invitationId,
-  //       inviteeColor: chance.pickone(['w', 'b'])
-  //     });
-  //     throw new Error('Should have failed.');
-  //   } catch (error) {
-  //     expect(error.response.errors[0].extensions.code).toStrictEqual('UNAUTHENTICATED');
-  //     expect(error.message).toContain('You must be logged in.');
-  //   }
-  // });
+    try {
+      await gqlClient.request(createGameMutation, {
+        invitationId: invitation.invitationId
+      });
+      throw new Error('Should have failed.');
+    } catch (error) {
+      expect(error.response.errors[0].extensions.code).toStrictEqual('UNAUTHENTICATED');
+      expect(error.message).toContain('You must be logged in.');
+    }
+  });
 });
