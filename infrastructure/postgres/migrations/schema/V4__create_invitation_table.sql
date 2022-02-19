a920165508db9346795f64ef7a4280f90f4c0eb1
Will Mallett <mallett002@gmail.com>
2022-02-19 10:28:02 -0600

    invitor color in db

--- infrastructure/postgres/migrations/schema/V4__create_invitation_table.sql
+++ infrastructure/postgres/migrations/schema/V4__create_invitation_table.sql
@@ -6,5 +6,5 @@ CREATE TABLE chess_club.tbl_invitation (
   invitation_id         UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
   invitor_id            UUID NOT NULL REFERENCES chess_club.tbl_player (player_id) ON DELETE CASCADE,
   invitee_id            UUID NOT NULL REFERENCES chess_club.tbl_player (player_id) ON DELETE CASCADE,
-  invitee_color         INVITATION_COLOR NOT NULL
+  invitor_color         INVITATION_COLOR NOT NULL
 );
